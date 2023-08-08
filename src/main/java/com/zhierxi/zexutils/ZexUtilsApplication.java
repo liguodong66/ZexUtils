94f6b56a6161e168ac402c0f1f8a89f2accd79cf
Gorden <addres>
2023-08-08 16:06:08 +0800

    inti

---
+++ src/main/java/com/zhierxi/zexutils/ZexUtilsApplication.java
@@ -0,0 +1,13 @@
+package com.zhierxi.zexutils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZexUtilsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZexUtilsApplication.class, args);
+    }
+
+}
